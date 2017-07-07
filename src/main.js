dddce240e0a5f7c3727b980a8be2a4e01e916af3
ikemura <k_ikemura@yumemi.co.jp>
2017-07-07 17:46:40 +0900

    import semantic js css

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import '../semantic/semantic.js'
+import '../semantic/semantic.css'
 
 Vue.config.productionTip = false
 
