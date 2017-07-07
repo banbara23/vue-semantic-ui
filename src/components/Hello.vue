fa9c18f00f8a6b0c95c10c34d234e16bf76ee966
ikemura <k_ikemura@yumemi.co.jp>
2017-07-07 17:47:14 +0900

    replace to semantic-ui html contents

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,29 +1,39 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div class="ui container">
+      <div class="ui labeled button" tabindex="0">
+        <div class="ui red button">
+          <i class="heart icon"></i> Like
+        </div>
+        <a class="ui basic red left pointing label">
+          1,048
+        </a>
+      </div>
+      <div class="ui labeled button" tabindex="0">
+        <div class="ui basic blue button">
+          <i class="fork icon"></i> Forks
+        </div>
+        <a class="ui basic left pointing blue label">
+          1,048
+        </a>
+      </div>
+      <div class="ui yellow progress" data-percent="60">
+        <div class="bar"></div>
+      </div>
+      <div class="ui olive progress" data-percent="30">
+        <div class="bar"></div>
+      </div>
+      <div class="ui green progress" data-percent="85">
+        <div class="bar"></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'hello',
-  data () {
+  data() {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
@@ -33,7 +43,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 
